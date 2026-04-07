6d74f30c7bec0885ae5a975d82166bfcdf279cb2
Jennifer Timmerbacka <Jennifer.Timmerbacka@abo.fi>
2026-04-07 13:51:45 +0300

    Add API authentication URLs to urlpatterns

--- backend/urls.py
+++ backend/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),  # make sure store/urls.py exists
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
