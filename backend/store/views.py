48f8db9ad3f9ca23501c38416d6557bbd63b0774
Jennifer Timmerbacka <Jennifer.Timmerbacka@abo.fi>
2026-04-07 14:06:46 +0300

    Add UserViewSet and GroupViewSet for API endpoints

--- backend/store/views.py
+++ backend/store/views.py
@@ -6,6 +6,24 @@ import random
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
 
+class UserViewSet (viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited
+    """
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permission.IsAuthenticated]
+    
+
+class GroupViewSet (viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 
 def home(request):
     return HttpResponse("Welcome to the Web shop!")
